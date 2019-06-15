a7c5b32f1675add57fef8fba4904808f58919b81
zhaoyiming1329988686 <1329988686@qq.com>
2019-06-15 11:46:01 +0800

    多表ok

---
+++ 结业项目/DAO/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class Class1
+    {
+
+        //多表方法
+        //public DataTable SelectClass2()
+        //{
+
+        //    var select1 = from e in mvc.StudentClass
+        //                  select new { e.ClassID, e.CLassName };
+        //    DataTable dt = new DataTable();
+        //    dt.Columns.Add("ClassID");
+        //    dt.Columns.Add("CLassName");
+
+        //    foreach (var item in select1)
+        //    {
+        //        DataRow dr = dt.NewRow();
+        //        dr["ClassID"] = item.ClassID.ToString();
+        //        dr["CLassName"] = item.CLassName.ToString();
+        //        dt.Rows.Add(dr);
+
+        //    }
+        //    return dt;
+        //}
+    }
+}
